13e339fb7a169e93727cd1fa1e20c6fe176215b5
khadija <khadija@kali.khadija>
2026-01-11 01:29:21 -0500

    Counter Recreated

---
+++ Counter02/README.md
@@ -0,0 +1,8 @@
+# simple counter
+## Ui Updation
+### Core React
+### Use of Use state Hook
+### Two main error Not in the logi but in the syntax
+#### used add_value() whih diretly executes the funtion irrespetive of on click event causing infinite loop rendering
+### 2nd used assignment operator instead of the Comparison operator
+### Rating 3/10
---
+++ Counter02/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+ 
+function App() {
+  const [count, setCount] = useState(10)
+   let Add_value=()=>{
+    if (count==20) {
+      setCount(count)
+    }else{
+    setCount(count+1)    
+   }}
+   let Remove_Value=()=>{
+   if (count==0) {
+    setCount(count)
+   }else{
+     setCount(count-1)
+   }}
+  return (
+   <>
+   <h1>React Hello</h1>
+   {/* Evaluated Expression { } */}
+   <h2>Counter  {count} </h2>
+   <button
+   onClick={Add_value} >
+    Add Value
+    {setCount} 
+    </button>
+    <button
+    onClick={Remove_Value}>
+      Remove Value {count}
+      </button>
+    <p>Counter Description{count} </p>
+   </>
+  )
+}
+
+export default App
