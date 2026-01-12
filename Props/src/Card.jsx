492e2f8ca77a7534bc29596a7dd8465a3d6c77df
khadija <khadija@kali.khadija>
2026-01-12 01:13:02 -0500

    Passed network image in Props

--- Props/src/Card.jsx
+++ Props/src/Card.jsx
@@ -1,10 +1,10 @@
 import './index.css'
-function Card({btn_text='Checkout'}) {
+function Card({btn_text='Checkout' ,image_url}) {
 
   return (
     <>
    <div className='bg-white border-2 h-80 p-1 w-72 border-slate-950 rounded-2xl justify-items-center'>
-   <img src="https://picsum.photos/300/200" alt="Random" className='rounded-2xl' />
+   <img src={image_url} alt="Random" className='rounded-2xl' />
    <p className='text-slate-950 font-bold pb-4' >
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
    </p>
